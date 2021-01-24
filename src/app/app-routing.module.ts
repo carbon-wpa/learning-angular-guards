fdf72c660cf183cf5a62c50f3c40af55657f04d0
Kacper Slotwinski <kacper.slotwinski@gmail.com>
2021-01-24 21:19:40 +0100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'user-account',
+    component: UserAccountComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
